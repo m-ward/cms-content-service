3dde6c221a8112dc4994dc683db34d7ad4e05aaa
Matthew Ward <matthew.ward@workiva.com>
2021-01-16 16:49:33 -0700

    springboot initializer

---
+++ src/main/java/com/cms/cmscontentservice/CmsContentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cms.cmscontentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsContentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsContentServiceApplication.class, args);
+	}
+
+}
